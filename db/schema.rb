bf31924005d80e79987bc8ff684787a35ba3887b
Chab Marion <marion.chabassol@essca.eu>
2017-02-07 15:28:24 +0100

    exo rails task manager

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 20170207102049) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
-    t.boolean  "done"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "done",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.string   "priority"
   end
 
